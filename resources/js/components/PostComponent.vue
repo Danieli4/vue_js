4edd63d2e0424999b982ec1970f91bf6a246db67
danilgryaznev <daniel58912007@gmail.com>
2023-03-15 22:20:15 +0300

    directives vue

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -3,6 +3,33 @@
         <SinglePostComponent></SinglePostComponent>
         <div> Name: {{name}}</div>
         <div> Job: {{vasyaJob}}</div>
+        <div>
+            {{persons}}
+        </div>
+        <div >
+            <table class="table">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Age</th>
+                    <th scope="col">Job</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="person in persons">
+                    <th scope="row">{{person.id}}</th>
+                    <td>{{person.name}}</td>
+                    <td>{{person.age}}</td>
+                    <td> {{person.job}}</td>
+                </tr>
+                </tbody>
+            </table>
+<!--            <div class="div"> {{person.name}}</div>-->
+<!--            <div class="div"> {{person.age}}</div>-->
+<!--            <div class="div"> {{person.job}}</div>-->
+
+        </div>
         <button @click="sayHello">Hello</button>
         <button @click="sayHi">Hi</button>
     </div>
@@ -15,18 +42,31 @@ export default {
 
     data() {
         return {
-            name: 'Vasia',
-            age: 20,
-            boolean: false,
-            someNull: null,
-            arr:[
-                [
+            persons:[
+                {
+                    id: 1,
+                    name: 'Vasya',
+                    age: 20,
+                    job: 'coach',
 
-                ]
-            ],
-            someObj:{
-                personName: 'Ivan'
-            }
+                },
+
+                {
+                    id: 2,
+                    name: 'Elena',
+                    age: 28,
+                    job: 'teacher',
+
+                },
+
+                {
+                    id: 3,
+                    name: 'Petya',
+                    age: 320,
+                    job: 'driver',
+
+                },
+            ]
         }
     },
 
