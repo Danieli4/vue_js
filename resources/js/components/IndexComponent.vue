79fbb8e466d86a878866c1df16d7420d607b3b65
danilgryaznev <daniel58912007@gmail.com>
2023-03-20 22:14:59 +0300

    Async CRUD. Update Person controller

--- resources/js/components/IndexComponent.vue
+++ resources/js/components/IndexComponent.vue
@@ -7,15 +7,26 @@
                 <th scope="col">Name</th>
                 <th scope="col">Age</th>
                 <th scope="col">Job</th>
+                <th scope="col">Edite</th>
             </tr>
             </thead>
             <tbody>
-            <tr v-for="person in people">
-                <th scope="row">{{person.id}}</th>
-                <td>{{person.name}}</td>
-                <td>{{person.age}}</td>
-                <td>{{person.job}}</td>
-            </tr>
+            <template v-for="person in people">
+                <tr :class="isEdit(person.id) ? 'd-none' : ''">
+                    <th scope="row">{{ person.id }}</th>
+                    <td>{{ person.name }}</td>
+                    <td>{{ person.age }}</td>
+                    <td>{{ person.job }}</td>
+                    <td><a href="#" @click.prevent="changeEditePersonId(person.id, person.name,  person.age, person.job)" class="btn btn-success">Edit</a></td>
+                </tr>
+                <tr :class="isEdit(person.id) ? '' : 'd-none'">
+                    <th scope="row">{{ person.id }}</th>
+                    <td><input type="text" class="form-control" v-model="name" ></td>
+                    <td><input type="number" class="form-control" v-model="age" ></td>
+                    <td><input type="text" class="form-control" v-model="job" ></td>
+                    <td><a href="#" @click.prevent="updatePerson(person.id)" class="btn btn-success">Update</a></td>
+                </tr>
+            </template>
             </tbody>
         </table>
     </div>
@@ -27,7 +38,13 @@ export default {
     name: "IndexComponent",
     data() {
         return {
-            people: null
+            people: null,
+            editPersonId: null,
+            name: '',
+            age: null,
+            job: '',
+
+
         }
     },
 
@@ -43,23 +60,25 @@ export default {
                     this.people = res.data;
                 });
         },
-
-        addPersons() {
-            axios.post('/api/people', {name: this.name, age: this.age, job: this.job})
-                .then(res =>{
-                    this.name =null,
-                    this.age =null,
-                    this.job =null,
-                    console.log(res);
+        updatePerson(id) {
+            this.editPersonId = null;
+            axios.patch(`/api/people/${id}`, {name: this.name, age: this.age, job: this.job})
+                .then(res=>{
+                    this.getPeople();
                 })
 
-            // axios.get('/persons')
-            //     .then(res => {
-            //         this.persons = res.data;
-            //         console.log(res);
-            //     })
-            //     .catch()
-            //     .finally();
+        },
+
+        changeEditePersonId(id, name, age, job) {
+            this.editPersonId = id;
+            this.name = name;
+            this.age = age;
+            this.job = job;
+        },
+
+        isEdit(id) {
+            return this.editPersonId === id;
+
         },
 
         //computed: {},
