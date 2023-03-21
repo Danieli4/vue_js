fc100ad8ac5e754b9094aeb7f1cdb2f7e8f84cbc
danilgryaznev <daniel58912007@gmail.com>
2023-03-21 21:15:55 +0300

    Async CRUD. Delete Person controller

--- resources/js/components/IndexComponent.vue
+++ resources/js/components/IndexComponent.vue
@@ -7,7 +7,8 @@
                 <th scope="col">Name</th>
                 <th scope="col">Age</th>
                 <th scope="col">Job</th>
-                <th scope="col">Edite</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Delete</th>
             </tr>
             </thead>
             <tbody>
@@ -18,6 +19,7 @@
                     <td>{{ person.age }}</td>
                     <td>{{ person.job }}</td>
                     <td><a href="#" @click.prevent="changeEditePersonId(person.id, person.name,  person.age, person.job)" class="btn btn-success">Edit</a></td>
+                    <td><a href="#" @click.prevent="deletePerson(person.id)" class="btn btn-danger">Delete</a></td>
                 </tr>
                 <tr :class="isEdit(person.id) ? '' : 'd-none'">
                     <th scope="row">{{ person.id }}</th>
@@ -64,6 +66,14 @@ export default {
             this.editPersonId = null;
             axios.patch(`/api/people/${id}`, {name: this.name, age: this.age, job: this.job})
                 .then(res=>{
+                    this.getPeople();
+                })
+
+        },
+
+        deletePerson(id) {
+            axios.delete(`/api/people/${id}`,)
+                .then(res=> {
                     this.getPeople();
                 })
 
